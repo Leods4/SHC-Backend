5146c71eda7b6ded9a51c6d7925c8e60ae4625b8
Leods4 <163310617+Leods4@users.noreply.github.com>
2025-11-24 08:01:08 -0300

    Update and rename 2025_11_18_103308_create_certificados_table.php to 2025_11_23_103308_create_certificados_table.php

--- database/migrations/2025_11_23_103308_create_certificados_table.php
+++ database/migrations/2025_11_23_103308_create_certificados_table.php
@@ -10,18 +10,30 @@ return new class extends Migration
     {
         Schema::create('certificados', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('aluno_id')->constrained('users')->cascadeOnDelete();
 
-            $table->string('categoria');
+            $table->foreignId('aluno_id')
+                ->constrained('users')
+                ->cascadeOnDelete();
+
+            // atualizado conforme o arquivo mudancas.txt
+            $table->foreignId('categoria_id')
+                ->constrained('categorias')
+                ->restrictOnDelete();
+
             $table->string('nome_certificado');
             $table->string('instituicao');
             $table->date('data_emissao');
             $table->unsignedInteger('carga_horaria_solicitada');
-            $table->string('arquivo_url'); // Caminho no storage
+            $table->string('arquivo_url'); // caminho no storage
 
-            $table->string('status')->default('ENTREGUE'); // ENTREGUE, APROVADO, REPROVADO, APROVADO_COM_RESSALVAS
+            $table->string('status')->default('ENTREGUE');
+            // ENTREGUE, APROVADO, REPROVADO, APROVADO_COM_RESSALVAS
 
-            $table->foreignId('coordenador_id')->nullable()->constrained('users')->nullOnDelete();
+            $table->foreignId('coordenador_id')
+                ->nullable()
+                ->constrained('users')
+                ->nullOnDelete();
+
             $table->unsignedInteger('horas_validadas')->nullable();
             $table->text('observacao')->nullable();
             $table->timestamp('data_validacao')->nullable();
